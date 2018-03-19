1319c5ed87cf2e06f950bf3e1b2e26a4d836a60e
Christopher Zumberge <chriszumberge@gmail.com>
2018-03-19 13:21:36 -0400

    added project files

---
+++ src/ZESoft.Azure.Mobile.DataStores/IBaseAzureStore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZESoft.Azure.Mobile.Models;
+
+namespace ZESoft.Azure.Mobile.DataStores
+{
+    public interface IBaseAzureStore<T> where T : class, IAzureDataObject, new()
+    {
+        string Identifier { get; }
+
+        Task<IEnumerable<T>> GetItemsAsync();
+        Task<T> GetItemAsync(string id);
+        Task<T> InsertAsync(T item);
+        Task<bool> UpdateAsync(T item);
+        Task<bool> DeleteAsync(string id);
+    }
+}
